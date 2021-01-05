74385138767c4075efbd1f58935344fc81017042
ExtraHash <ExtraHash@protonmail.com>
2021-01-04 20:58:32 -0700

    don't await close

--- src/__tests__/Client.ts
+++ src/__tests__/Client.ts
@@ -202,7 +202,7 @@ describe("Perform client tests", () => {
         // at node_modules/tarn/dist/Pool.js:207:53
 
         // TODO: figure out how to close whatever this is properly
-        await client.close();
+        client.close();
 
         done();
     });
